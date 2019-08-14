919f22b673101f30a4c4cb87cb9909abd0bd0760
jieqiongbenyu <1072519895@qq.com>
2019-08-14 21:30:05 +0800

    利用providerData标签，根据不同的参数名，传递给方法不同的参数

--- TestNGChapter/src/main/java/com/course/testng/parameter/DataProviderTest.java
+++ TestNGChapter/src/main/java/com/course/testng/parameter/DataProviderTest.java
@@ -3,6 +3,8 @@ package com.course.testng.parameter;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Method;
+
 public class DataProviderTest {
 
     @Test(dataProvider = "data")
@@ -19,5 +21,34 @@ public class DataProviderTest {
         };
         return o;
     }
+
+    @Test(dataProvider = "method")
+    public void test1(String name, int age){
+        System.out.println("test1111111111" + " " + name + " " + age);
+    }
+
+    @Test(dataProvider = "method")
+    public void test2(String name, int age){
+        System.out.println("test22222222222" + " " + name + " " + age);
+    }
+
+    @DataProvider(name = "method")
+    public Object[][] testDataProvider1(Method method){
+        Object[][] result = null;
+        if (method.getName().equals("test1")){
+            result = new Object[][]{
+                    {"test1", 10},
+                    {"test11", 20}
+            };
+
+        }else if (method.getName().equals("test2")){
+            result = new Object[][]{
+                    {"test2", 30},
+                    {"test22", 40}
+            };
+        }
+        return result;
+    }
+
 
 }
