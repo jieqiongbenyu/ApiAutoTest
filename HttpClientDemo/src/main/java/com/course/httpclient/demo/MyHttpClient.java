93f7e0f6150f4552e50ec67815f2eb2d561a9889
jieqiongbenyu <1072519895@qq.com>
2019-08-25 17:51:28 +0800

    增加一个httpclient demo

---
+++ HttpClientDemo/src/main/java/com/course/httpclient/demo/MyHttpClient.java
@@ -0,0 +1,28 @@
+package com.course.httpclient.demo;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class MyHttpClient {
+
+    @Test
+    public void test1() throws IOException {
+        //用来存放结果
+        String result;
+        SSLSocketFactory.getSocketFactory().setHostnameVerifier(new AllowAllHostnameVerifier());
+        HttpGet get = new HttpGet("https://www.baidu.com");
+        //用来执行上面的get方法
+        DefaultHttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(get);
+        result = EntityUtils.toString(response.getEntity(),"utf-8");
+        System.out.println(result);
+    }
+}
