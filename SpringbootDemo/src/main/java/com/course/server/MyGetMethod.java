4c845f60f06cec151d34b0ceb107544cf2ea7f4f
jieqiongbenyu <1072519895@qq.com>
2019-09-16 21:48:31 +0800

    增加get请求返回cookies信息的方法

--- SpringbootDemo/src/main/java/com/course/server/MyGetMethod.java
+++ SpringbootDemo/src/main/java/com/course/server/MyGetMethod.java
@@ -4,11 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 public class MyGetMethod {
 
     @RequestMapping(value = "/get/cookies", method = RequestMethod.GET)
-    public String getCookies(){
+    public String getCookies(HttpServletResponse response){
+        Cookie cookies = new Cookie("login", "true1");
+        response.addCookie(cookies);
         return "获取cookies成功了啊";
     }
 }
